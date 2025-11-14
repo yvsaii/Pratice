a99905953aa5f5ee0360103446aaf623673ce5aa
Yarraboina Venkatasai <yarraboinavenkatasai@gmail.com>
2025-11-14 09:58:13 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-// import Login from './Components/Login'
 import Home from './Components/Home'
-// import Aboutus from './Components/Aboutus'
-import Registration from './Components/Registration'
 import {Route ,  Routes, BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Registration />} />
+      <Route path='/' element={<Home />} />
+   
     </Routes>
    </BrowserRouter>
   );
